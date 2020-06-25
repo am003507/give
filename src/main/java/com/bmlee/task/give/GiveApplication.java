d0c3334a091bbe1b51a2a85e17b83879c06d74c0
bmlee <am003507>
2020-06-25 18:38:21 +0900

    minor - README.MD, logback.xml, gitignore 추가, assertJ 의존성 추가

--- src/main/java/com/bmlee/task/give/GiveApplication.java
+++ src/main/java/com/bmlee/task/give/GiveApplication.java
@@ -1,13 +1,14 @@
 package com.bmlee.task.give;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class GiveApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GiveApplication.class, args);
     }
-
 }
