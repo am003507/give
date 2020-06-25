b072d7b8624d174ed2ebbb49129497b9f0146843
bmlee <am003507>
2020-06-25 18:09:44 +0900

    프로젝트 초기화

---
+++ src/test/java/com/bmlee/task/give/GiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmlee.task.give;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
